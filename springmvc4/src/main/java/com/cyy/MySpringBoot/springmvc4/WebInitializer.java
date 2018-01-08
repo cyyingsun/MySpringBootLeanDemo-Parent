146a3b6605a4bde947018362141ac3d4c5091ca4
chengyingying <p_chengyingying@onelinkplus.com>
2018-01-08 18:11:18 +0800

    springmvc4

---
+++ springmvc4/src/main/java/com/cyy/MySpringBoot/springmvc4/WebInitializer.java
@@ -0,0 +1,29 @@
+package com.cyy.MySpringBoot.springmvc4;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// TODO Auto-generated method stub
+		AnnotationConfigWebApplicationContext context = 
+				new AnnotationConfigWebApplicationContext();
+		context.register(MyMvcConfig.class);
+		context.setServletContext(servletContext);
+		
+		Dynamic servlet = servletContext.addServlet("dispatcher", 
+				new DispatcherServlet(context));
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+		
+	}
+
+}
---
+++ springmvc4/src/main/java/com/cyy/MySpringBoot/springmvc4/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.cyy.MySpringBoot.springmvc4.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String hello(){
+		return "index";
+	}
+}
